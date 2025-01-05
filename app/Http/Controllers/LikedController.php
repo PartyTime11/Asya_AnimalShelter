255acff2162f501b72f67d3dad873039d8fe4ab7
purpurrya <anastasia.tikhonenko.meow@gmail.com>
2025-01-05 18:39:22 +0300

    fix request for animal_id

--- app/Http/Controllers/LikedController.php
+++ app/Http/Controllers/LikedController.php
@@ -7,12 +7,22 @@ use Illuminate\Support\Facades\DB;
 
 class LikedController extends Controller
 {
-    public function addOrDeleteFavorite(Request $request, $animal_id)
+    public function addOrDeleteFavorite(Request $request)
     {
+        $request->validate([
+            'user_token' => 'required|string',
+            'animal_id' => 'required|integer',
+        ]);
+        
+        $animal_id = $request->input('animal_id');
         $user_token = $request->input('user_token');
 
         if (!$user_token) {
             return response()->json(['message' => 'Токен не получен.'], 400);
+        }
+
+        if (!$animal_id) {
+            return response()->json(['message' => 'ID животного не получено.'], 400);
         }
 
         $favorite = DB::table('favorites')
